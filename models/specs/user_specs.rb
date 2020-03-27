4d2e192460e5a815c8480c6e249bf115e0ca7c5d
fierytortilla <dschlessinger@live.com>
2020-03-27 00:00:07 +0000

    added new uml design

--- models/specs/user_specs.rb
+++ models/specs/user_specs.rb
@@ -3,11 +3,12 @@ require('minitest/reporters')
 Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
 
 require_relative("../user")
+require_relative("../music_item")
 
 class UserTest< MiniTest::Test
 
   def setup()
-
+    @user = User.new(({'user_name'=>'Rainger FX', 'email_address'=> 'david@rainger.com', 'active_flag'=>true})
   end
 
   def test_user_buying()
